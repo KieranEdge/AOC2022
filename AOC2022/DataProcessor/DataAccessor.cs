32923123c3e975eba892ab208c2696c5d3b4f3c8
Kieran Edge <kesedge94@gmail.com>
2025-07-30 19:59:51 +0100

    Day 1 Part 1
    Initial Commit for working day 1

---
+++ AOC2022/DataProcessor/DataAccessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2022_1.DataProcessor
+{
+    public static class DataAccessor
+    {
+        public static void FileReader(string filePath)
+        {
+            // Initialising the streamreader and results list
+            StreamReader sr = new StreamReader(filePath);
+            List<int> resultsList = new List<int>();
+
+            // Reading the first line and parsing it to an int
+            string line = sr.ReadLine();
+            int rollingSum = int.Parse(line);
+
+            // Continuing to read the lines
+            while (line != null)
+            {
+                // Read the next line
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+                // If it's not an empty string, add to the rolling sum
+                if (line != "" && line != null)
+                {
+                    rollingSum += int.Parse(line);
+                }
+
+                // If it's a gap add the rolling sum to the list and reset the sum to zero
+                else
+                {
+                    resultsList.Add(rollingSum);
+                    rollingSum = 0;
+                }
+            }
+
+            sr.Close();
+
+            Console.WriteLine(resultsList.Max());
+
+        }
+    }
+}
---
+++ AOC2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using AOC2022_1.DataProcessor;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string filePath = "C:\\Users\\Kieran Edge\\source\\repos\\AOC2022\\AOC2022\\Data\\Data.txt";
+
+DataAccessor.FileReader(filePath);
+
+/*TO DO
+ * Read data from file line by line and convert to int
+ * initialise a list of sums
+ * Add int to rolling sum
+ * If blank line set commit rolling sum to zero and add previous sum to the list
+ * Return max value in the list
+ */
