d8d32354f80c6e0bb449753b61c0fbeb3e368c86
Kieran Edge <kesedge94@gmail.com>
2025-08-06 21:35:34 +0100

    Day 4 Part 2 Complete

--- AOC2022_4/Data Processor/DataProcessor.cs
+++ AOC2022_4/Data Processor/DataProcessor.cs
@@ -8,7 +8,7 @@ namespace AOC2022_4.Data_Processor
 {
     public static class DataProcessor
     {
-        public static bool IntegerComparer(int seq1_start, int seq1_end, int seq2_start, int seq2_end)
+        public static bool IntegerComparer_Part1(int seq1_start, int seq1_end, int seq2_start, int seq2_end)
         {
             bool result = false;
 
@@ -18,6 +18,48 @@ namespace AOC2022_4.Data_Processor
             }
             else if (seq2_start >= seq1_start && seq2_end <= seq1_end)
             {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public static bool IntegerComparer_Part2(int seq1_start, int seq1_end, int seq2_start, int seq2_end)
+        {
+            bool result = false;
+
+            if (seq1_start >= seq2_start && seq1_end <= seq2_end)
+            {
+                // seq 1 fully contained within seq 2
+                result = true;
+            }
+            else if (seq2_start >= seq1_start && seq2_end <= seq1_end)
+            {
+                // seq 2 fully contained within seq 1
+                result = true;
+            }
+
+            else if (seq1_start < seq2_start && seq1_end >= seq2_start && seq1_end <= seq2_end)
+            {
+                // seq1 overlaps seq 2 from the outside and into it
+                result = true;
+            }
+
+            else if(seq1_start >= seq2_start && seq1_start <= seq2_end && seq1_end > seq2_end)
+            {
+                // seq1 overlaps seq 2 from inside it and out of it
+                result = true;
+            }
+
+            else if (seq2_start < seq1_start && seq2_end >= seq1_start && seq2_end <= seq1_end)
+            {
+                // seq2 overlaps seq 1 from the outside and into it
+                result = true;
+            }
+
+            else if (seq2_start >= seq1_start && seq2_start <= seq1_end && seq2_end > seq1_end)
+            {
+                // seq2 overlaps seq 1 from inside it and out of it
                 result = true;
             }
 
--- AOC2022_4/Program.cs
+++ AOC2022_4/Program.cs
@@ -5,7 +5,8 @@ string filePath = "C:\\Users\\Kieran Edge\\source\\repos\\AOC2022\\AOC2022_4\\Da
 
 List<int[,]> sequence1 = new List<int[,]>();
 List<int[,]> sequence2 = new List<int[,]>();
-int runningTotal = 0;
+int runningTotal_Part1 = 0;
+int runningTotal_Part2 = 0;
 
 (sequence1, sequence2) = DataAccessor.TextFileToIntegerArrays(filePath);
 
@@ -16,11 +17,18 @@ for (int i = 0; i < sequence1.Count; i++)
     int sequence2Start = sequence2[i][0,0];
     int sequence2End = sequence2[i][1,0];
 
-    bool result = DataProcessor.IntegerComparer(sequence1Start, sequence1End, sequence2Start, sequence2End);
-    if (result)
+    bool result_1 = DataProcessor.IntegerComparer_Part1(sequence1Start, sequence1End, sequence2Start, sequence2End);
+    if (result_1)
     {
-        runningTotal++;
+        runningTotal_Part1++;
+    }
+
+    bool result_2 = DataProcessor.IntegerComparer_Part2(sequence1Start, sequence1End, sequence2Start, sequence2End);
+    if (result_2) 
+    { 
+        runningTotal_Part2++; 
     }
 }
 
-Console.WriteLine($"Number of sequences contained = {runningTotal}");
+Console.WriteLine($"Number of sequences fully contained = {runningTotal_Part1}");
+Console.WriteLine($"Number of sequences partially contained = {runningTotal_Part2}");
