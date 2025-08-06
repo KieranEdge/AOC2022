5438777f8987b66072dd33b6598a8c65afababc4
Kieran Edge <kesedge94@gmail.com>
2025-08-06 21:25:54 +0100

    Day 4 - Part 1 Complete

---
+++ AOC2022_4/Data Processor/DataProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2022_4.Data_Processor
+{
+    public static class DataProcessor
+    {
+        public static bool IntegerComparer(int seq1_start, int seq1_end, int seq2_start, int seq2_end)
+        {
+            bool result = false;
+
+            if (seq1_start >= seq2_start && seq1_end <= seq2_end)
+            {
+                result = true;
+            }
+            else if (seq2_start >= seq1_start && seq2_end <= seq1_end)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
--- AOC2022_4/Program.cs
+++ AOC2022_4/Program.cs
@@ -1,8 +1,26 @@
 ﻿using AOC2022_4.Data_Accessor;
+using AOC2022_4.Data_Processor;
 
 string filePath = "C:\\Users\\Kieran Edge\\source\\repos\\AOC2022\\AOC2022_4\\Data\\Data.txt";
 
 List<int[,]> sequence1 = new List<int[,]>();
 List<int[,]> sequence2 = new List<int[,]>();
+int runningTotal = 0;
 
 (sequence1, sequence2) = DataAccessor.TextFileToIntegerArrays(filePath);
+
+for (int i = 0; i < sequence1.Count; i++)
+{
+    int sequence1Start = sequence1[i][0,0];
+    int sequence1End = sequence1[i][1,0];
+    int sequence2Start = sequence2[i][0,0];
+    int sequence2End = sequence2[i][1,0];
+
+    bool result = DataProcessor.IntegerComparer(sequence1Start, sequence1End, sequence2Start, sequence2End);
+    if (result)
+    {
+        runningTotal++;
+    }
+}
+
+Console.WriteLine($"Number of sequences contained = {runningTotal}");
