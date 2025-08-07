e022125c25a485d9e6d1921e3ed361d80a021031
Kieran Edge <kesedge94@gmail.com>
2025-08-07 18:56:11 +0100

    Day 6 part 1 complete

---
+++ AOC2022_6/Data_Accessor/Data_Accessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2022_6.Data_Accessor
+{
+    public static class Data_Accessor
+    {
+        public static char[] TextFileToCharacterArray(string filePath)
+        {
+            string allText = File.ReadAllText(filePath);
+            List<char> characters = new List<char>();
+            foreach (char c in allText)
+            {
+                characters.Add(c);
+            }
+            return characters.ToArray();
+        }
+    }
+}
---
+++ AOC2022_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using AOC2022_6.Data_Accessor;
+
+string filePath = "C:\\Users\\Kieran Edge\\source\\repos\\AOC2022\\AOC2022_6\\Data\\Data.txt";
+
+char[] charactersInFile = Data_Accessor.TextFileToCharacterArray(filePath);
+
+Console.WriteLine(charactersInFile[0]);
+List<char> recentCharacters = new List<char>();
+int processingInt = 0;
+bool stopProcessing = false;
+
+while (!stopProcessing)
+{
+    if (recentCharacters.Contains(charactersInFile[processingInt]))
+    {
+        // Clearing the list and adding the new character
+        recentCharacters.Clear();
+        recentCharacters.Add(charactersInFile[processingInt]);
+        processingInt++;
+    }
+    else
+    {
+        // Adding the new character
+        recentCharacters.Add(charactersInFile[processingInt]);
+        if (recentCharacters.Count == 4)
+        {
+            break;
+        }
+        processingInt++;
+    }
+}
+
+Console.WriteLine(processingInt + 1);
