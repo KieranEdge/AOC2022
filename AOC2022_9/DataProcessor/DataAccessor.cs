94006d021167808df4a367b2b3893541be2a8013
Kieran Edge <kesedge94@gmail.com>
2025-08-10 23:46:31 +0100

    Day 9 Head Travel mapped out

---
+++ AOC2022_9/DataProcessor/DataAccessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2022_9.DataProcessor
+{
+    internal static class DataAccessor
+    {
+        public static (List<char>, List<int>) fileToDirectionsAndMoves(string filePath)
+        {
+            List<char> directionList= new List<char>();
+            List<int> movesList = new List<int>();
+            string[] linesInFile = File.ReadAllLines(filePath);
+
+            foreach (string line in linesInFile)
+            {
+                directionList.Add(Char.Parse(line.Split(' ')[0]));
+                movesList.Add(int.Parse(line.Split(" ")[1]));
+            }
+
+            return (directionList, movesList);
+        }
+    }
+}
---
+++ AOC2022_9/Program.cs
@@ -0,0 +1,42 @@
+﻿using AOC2022_9.DataProcessor;
+
+List<char> directionsList = new List<char>();
+List<int> movesList  = new List<int>();
+string filePath = "C:\\Users\\Kieran Edge\\source\\repos\\AOC2022\\AOC2022_9\\Data\\Data.txt";
+
+(directionsList, movesList) = DataAccessor.fileToDirectionsAndMoves(filePath);
+
+List<List<int>> headMoves = new List<List<int>>();
+List<List<int>> tailMoves = new List<List<int>>();
+
+List<int> headPosition = new List<int>() { 0, 0 };
+List<int> tailPosition = new List<int>() { 0, 0 };
+
+// Iterating over the directions
+for(int i = 0;  i < directionsList.Count; i++)
+{
+    int numberOfSteps = movesList[i];
+
+    for(int j = 0; j < numberOfSteps; j++)
+    {
+        if (directionsList[i] == 'U')
+        {
+            headPosition[1]++;
+        }
+        else if (directionsList[i] == 'D')
+        {
+            headPosition[1]--;
+        }
+        else if (directionsList[i] == 'R')
+        {
+            headPosition[0]++;
+        }
+        else if (directionsList[i] == 'L')
+        {
+            headPosition[0]--;
+        }
+        Console.WriteLine($"Head is at position: {headPosition[0]}, {headPosition[1]}");
+        headMoves.Add(headPosition);
+    }
+}
+
